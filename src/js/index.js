1e72c4d25db30e70fe6a000b4a905b5836b4f56d
Alamin <codepapa360@gmail.com>
2023-05-18 03:47:09 +0600

    Structuring

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,61 @@
 "use strict";
 import "../sass/main.scss";
+import rawJson from "../json/data.json";
 
 const btnDaily = document.querySelector(".btn-daily");
 const btnWeekly = document.querySelector(".btn-weekly");
 const btnMonthly = document.querySelector(".btn-monthly");
 
-const renderWork = function (data) {
-  const parentEl = document.querySelector(".work");
+//Render output
+const renderOutput = function (data, parentEl) {
   const hours = parentEl.querySelector(".report__content__info__hours");
-  console.log(hours);
+  const prevInfo = parentEl.querySelector(".report__content__prev-info");
+
+  hours.textContent = data.currentHrs;
+  prevInfo.textContent = `${data.period} - ${data.previousHrs}hrs`;
+};
+
+const renderData = function (receivedData) {
+  const workParentEl = document.querySelector(".work");
+  const playParentEl = document.querySelector(".play");
+  const studyParentEl = document.querySelector(".study");
+  const exerciseParentEl = document.querySelector(".exercise");
+  const socialParentEl = document.querySelector(".social");
+  const selfcareParentEl = document.querySelector(".self-care");
+
+  renderOutput(receivedData.work, workParentEl);
+};
+
+const handleData = async function (status) {
+  try {
+    const rawData = await fetch(rawJson).then((res) => res.json());
+
+    console.log(rawData);
+
+    const readyData = {
+      work: {
+        daily: {
+          currentHrs: null,
+          previousHrs: null,
+          period: "Today",
+        },
+      },
+      play: {
+        currentHrs: null,
+        previousHrs: null,
+        period: "Today",
+      },
+    };
+
+    if (status === "daily") {
+      renderData(readyData);
+    }
+
+    return renderData(readyData);
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    throw error;
+  }
 };
 
 btnDaily.addEventListener("click", function () {
@@ -18,7 +65,7 @@ btnDaily.addEventListener("click", function () {
 
   this.classList.add("btn-active");
 
-  renderWork();
+  handleData("daily");
 });
 
 btnWeekly.addEventListener("click", function () {
